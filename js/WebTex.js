f8f3c96cfafe9701dc596f5a6fd4bf2d77c70270
xlambein <xavier.lambein@student.uclouvain.be>
2014-05-06 23:22:37 +0200

    Created & implemented WebTex for rendering

---
+++ js/WebTex.js
@@ -0,0 +1,37 @@
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: false, // IMPORTANT, because we do MathJax before markdown,
+                   //            however we do escaping in 'CreatePreview'.
+  smartLists: true,
+  smartypants: false
+});
+
+var WebTex = {
+  escape: function (html, encode) {
+    return html
+      .replace(!encode ? /&(?!#?\w+;)/g : /&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+     .replace(/'/g, '&#39;');
+  },
+  
+  markdown: function (target) {
+    var html = marked(target.innerHTML);
+    target.innerHTML = html;
+  },
+  
+  render: function (sourceCode, target, callback) {
+    target.innerHTML = this.escape(sourceCode);
+    
+    MathJax.Hub.Queue(
+      ["Typeset", MathJax.Hub, target],
+      ["markdown", this, target],
+      [callback]
+    );
+  },
+};
